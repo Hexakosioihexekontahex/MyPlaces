b3013b13a76540bff39b4858388da70efcaf5d20
Hex <wolfgunmiller@gmail.com>
2020-02-18 17:47:10 +0300

    Place Model temp Place-s

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,37 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Roman Melnik on 18.02.2020.
+//  Copyright © 2020 Roman Melnik. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+
+    static let restaurantNames = [
+            "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+            "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+            "Speak Easy", "Morris Pub", "Вкусные истории",
+            "Классик", "Love&Life", "Шок", "Бочка"
+        ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+}
