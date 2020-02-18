e2e9b19cca8716ab8f10d4f0f3f4acc1c64db4bf
Hex <wolfgunmiller@gmail.com>
2020-02-18 11:14:51 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Roman Melnik on 18.02.2020.
+//  Copyright © 2020 Roman Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
